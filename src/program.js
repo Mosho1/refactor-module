c0f71ed0f69c6931f15e086861212d85fc791591
Sharon Rolel <sharonr@tipranks.com>
2016-02-07 20:53:31 +0200

    .

--- src/program.js
+++ src/program.js
@@ -7,19 +7,13 @@ program
   .command('generate-tree <entry>')
   .option('-o, --out <value>', 'set output file')
   .description('generate the initial module tree')
-  .action((entry, {out}) => {
-    commands.generateTree(entry, {out});
-    process.exit(0);
-  });
-
+  .action(commands.generateTree);
+  
 program
   .command('refactor <entry>')
   .description('refactor the module')
   .option('-o, --out <value>', 'set output directory')
   .option('-t, --tree <value>', 'set tree file location')
-  .action((entry, {tree, out}) => {
-    commands.refactor(entry, {tree, out});
-    process.exit(0);
-  });
+  .action(commands.refactor);
 
 program.parse(process.argv);
