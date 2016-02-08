02bf9ef4b73689ef8653c7a98e42c9b84bdc15ff
Sharon Rolel <sharonr@tipranks.com>
2016-02-08 14:19:48 +0200

    added local babel support

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-require('babel-register');
+const relative = require('require-relative');
+var register;
+try {
+  register = relative('babel-register', process.cwd());
+} catch(e1) {
+  console.log(`can't find babel-register, trying babel-core/register...`);
+  try {
+    register = relative('babel-core/register', process.cwd());
+  } catch(e2) {
+    console.log(`can't find babel-core/register, using globally installed babel-register...`);
+    try {
+      register = require('babel-register');
+    } catch(e3) {
+      console.error(e3);
+    }
+  }
+}
+
 require('./build/program');
